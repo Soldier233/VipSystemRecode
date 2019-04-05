9b9f8abf9afa2db8f8021402101463be12d3e1fe
Soldier <1224954468@qq.com>
2019-04-05 22:40:31 +0800

    initial commit

---
+++ src/main/java/me/zhanshi123/vipsystem/Main.java
@@ -0,0 +1,23 @@
+package me.zhanshi123.vipsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    private static Main instance;
+
+
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/zhanshi123/vipsystem/MessageManager.java
@@ -0,0 +1,75 @@
+package me.zhanshi123.vipsystem;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MessageManager {
+    private static FileConfiguration config = new YamlConfiguration();
+    private static File f = null;
+
+    private static void writeFile(InputStream input, File file) {
+        try {
+            int index;
+            byte[] bytes = new byte[10240];
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            while ((index = input.read(bytes)) != -1) {
+                fileOutputStream.write(bytes, 0, index);
+                fileOutputStream.flush();
+            }
+            fileOutputStream.close();
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void init() {
+        try {
+            File folder = new File(Main.getInstance().getDataFolder(), "messages");
+            folder.mkdirs();
+            f = new File(Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "messages" + File.separator + Main.getConfigManager().getLanguage() + ".yml");
+            if (!f.exists()) {
+                InputStream inputStream = Main.getInstance().getResource(Main.getConfigManager().getLanguage() + ".yml");
+                if (inputStream != null) {
+                    writeFile(inputStream, f);
+                } else {
+                    inputStream = Main.getInstance().getResource("en.yml");
+                    f = new File(Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "messages" + File.separator + Main.getConfigManager().getLanguage() + ".yml");
+                    writeFile(inputStream, f);
+                }
+            }
+            config.load(new InputStreamReader(new FileInputStream(f), "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getString(String path) {
+        String str = config.getString(path);
+        if (str == null) {
+            return "MISSING_LANGUAGE";
+        }
+        return str.replace("&", "§");
+    }
+
+    public static List<String> getStringList(String path) {
+        List<String> array = config.getStringList(path);
+        if (array == null) {
+            array = Arrays.asList(new String[]{"MISSING_LANGUAGE"});
+        }
+        List<String> temp = new ArrayList<>();
+        array.forEach(str -> temp.add(str.replace("&", "§")));
+        return temp;
+    }
+}
