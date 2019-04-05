fca6996dbc4973a7d3d2dfe28a2464a7ddd9303f
Soldier <1224954468@qq.com>
2019-04-05 22:46:17 +0800

    update util

---
+++ src/main/java/me/zhanshi123/vipsystem/manager/UpdateManager.java
@@ -0,0 +1,16 @@
+package me.zhanshi123.vipsystem.manager;
+
+import me.zhanshi123.vipsystem.util.Update;
+import org.bukkit.entity.Player;
+
+public class UpdateManager {
+    private Update update;
+
+    public Update getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Update update) {
+        this.update = update;
+    }
+}
---
+++ src/main/java/me/zhanshi123/vipsystem/task/UpdateCheckTask.java
@@ -0,0 +1,10 @@
+package me.zhanshi123.vipsystem.task;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class UpdateCheckTask extends BukkitRunnable{
+    @Override
+    public void run() {
+
+    }
+}
