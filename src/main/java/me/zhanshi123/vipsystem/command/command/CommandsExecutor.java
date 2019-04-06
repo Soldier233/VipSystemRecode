92ada513f3919c41f89988f3a1ead418fc25d8e0
Soldier <1224954468@qq.com>
2019-04-06 21:37:01 +0800

    add permissionCommand

--- src/main/java/me/zhanshi123/vipsystem/command/command/CommandsExecutor.java
+++ src/main/java/me/zhanshi123/vipsystem/command/command/CommandsExecutor.java
@@ -2,6 +2,7 @@ package me.zhanshi123.vipsystem.command.command;
 
 import me.zhanshi123.vipsystem.Main;
 import me.zhanshi123.vipsystem.command.command.type.AdminCommand;
+import me.zhanshi123.vipsystem.command.command.type.PermissionCommand;
 import me.zhanshi123.vipsystem.manager.MessageManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,7 +12,7 @@ public class CommandsExecutor implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (args.length == 0) {
             sendHelp(commandSender);
-            return false;
+            return true;
         } else {
             String arg = args[0];
             SubCommand subCommand = Main.getCommandHandler().getSubCommand(arg);
@@ -19,7 +20,7 @@ public class CommandsExecutor implements CommandExecutor {
                 subCommand = Main.getCommandHandler().getSubCommand("*");
             }
             if (subCommand == null) {
-                return false;
+                return true;
             }
             if (subCommand instanceof AdminCommand) {
                 if (commandSender.isOp()) {
@@ -27,7 +28,7 @@ public class CommandsExecutor implements CommandExecutor {
                         int argLength = subCommand.getArgLength();
                         if (argLength != args.length) {
                             subCommand.sendHelp(commandSender);
-                            return false;
+                            return true;
                         } else {
                             return subCommand.onCommand(commandSender, command, label, args);
                         }
@@ -35,15 +36,33 @@ public class CommandsExecutor implements CommandExecutor {
                         return subCommand.onCommand(commandSender, command, label, args);
                     }
                 } else {
-                    commandSender.sendMessage(MessageManager.getString(""));
-                    return false;
+                    commandSender.sendMessage(MessageManager.getString("permissionDeny"));
+                    return true;
                 }
+            } else if (subCommand instanceof PermissionCommand){
+                if(Main.getPermission().has(commandSender,"vipsystem."+subCommand.getName())){
+                    if (subCommand.isNeedArg()) {
+                        int argLength = subCommand.getArgLength();
+                        if (argLength != args.length) {
+                            subCommand.sendHelp(commandSender);
+                            return true;
+                        } else {
+                            return subCommand.onCommand(commandSender, command, label, args);
+                        }
+                    } else {
+                        return subCommand.onCommand(commandSender, command, label, args);
+                    }
+                }else{
+                    commandSender.sendMessage(MessageManager.getString("permissionDeny"));
+                    return true;
+                }
+
             } else {
                 if (subCommand.isNeedArg()) {
                     int argLength = subCommand.getArgLength();
                     if (argLength != args.length) {
                         subCommand.sendHelp(commandSender);
-                        return false;
+                        return true;
                     } else {
                         return subCommand.onCommand(commandSender, command, label, args);
                     }
