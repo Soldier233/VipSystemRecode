d93612f6f3f1b3e53b0f60e401e6943ea1047308
Soldier <1224954468@qq.com>
2019-04-19 00:01:01 +0800

    api update

--- src/main/java/me/zhanshi123/vipsystem/api/event/VipActivateEvent.java
+++ src/main/java/me/zhanshi123/vipsystem/api/event/VipActivateEvent.java
@@ -4,7 +4,23 @@ import me.zhanshi123.vipsystem.api.vip.VipData;
 import org.bukkit.entity.Player;
 
 public class VipActivateEvent extends VipEvent {
+    private boolean isNative = true;
+
     public VipActivateEvent(Player player, VipData vipData) {
         super(player, vipData);
+    }
+
+    /**
+     * @param player
+     * @param vipData
+     * @param isNative if is called by plugin itself
+     */
+    public VipActivateEvent(Player player, VipData vipData, boolean isNative) {
+        super(player, vipData);
+        this.isNative = isNative;
+    }
+
+    public boolean isNative() {
+        return isNative;
     }
 }
