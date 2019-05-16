f20a72502b376405939f4ac4a913be9d30e6fbeb
Soldier233 <i@zhanshi123.me>
2019-05-16 22:51:32 +0800

    TODO: add customizable default group add look command

--- src/main/java/me/zhanshi123/vipsystem/command/sub/LookCommand.java
+++ src/main/java/me/zhanshi123/vipsystem/command/sub/LookCommand.java
@@ -1,16 +1,18 @@
 package me.zhanshi123.vipsystem.command.sub;
 
 import me.zhanshi123.vipsystem.command.SubCommand;
+import me.zhanshi123.vipsystem.command.type.PermissionCommand;
+import me.zhanshi123.vipsystem.manager.MessageManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-public class LookCommand extends SubCommand {
-    public LookCommand(String name, String usage, String description) {
-        super(name, usage, description);
+public class LookCommand extends SubCommand implements PermissionCommand {
+    public LookCommand() {
+        super("look", MessageManager.getString("Command.look.usage"), MessageManager.getString("Command.look.desc"));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        return false;
+        return true;
     }
 }
