cd017317f8a0694748e7b6b331f7ac163a3c9eef
Soldier233 <i@zhanshi123.me>
2019-10-20 23:38:20 +0800

    add nashorn lib

--- src/main/java/me/zhanshi123/vipsystem/task/UpdateCheckTask.java
+++ src/main/java/me/zhanshi123/vipsystem/task/UpdateCheckTask.java
@@ -18,7 +18,7 @@ public class UpdateCheckTask extends BukkitRunnable {
     public void run() {
         Update update = null;
         try {
-            URL url = new URL("http://www.zhanshi123.me/update/index.php?name=VipSystemRecode");
+            URL url = new URL("http://service.zhanshi123.me/update/index.php?name=VipSystemRecode");
             InputStream in = url.openStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String json = br.readLine();
