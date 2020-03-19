009779e509f6b28c8eb0eeff07001ff067a7a89e
Soldier233 <i@zhanshi123.me>
2020-03-19 14:17:19 +0800

    add delay when joining

--- src/main/java/me/zhanshi123/vipsystem/listener/PlayerListener.java
+++ src/main/java/me/zhanshi123/vipsystem/listener/PlayerListener.java
@@ -5,10 +5,16 @@ import me.zhanshi123.vipsystem.task.CheckVipTask;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        new CheckVipTask(e.getPlayer()).runTask(Main.getInstance());
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                new CheckVipTask(e.getPlayer()).runTask(Main.getInstance());
+            }
+        }.runTaskLater(Main.getInstance(), 40L);
     }
 }
