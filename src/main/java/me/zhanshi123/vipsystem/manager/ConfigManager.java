1f742a1a884a1ff2417696c12fecbc878c08ccc1
Soldier233 <i@zhanshi123.me>
2021-02-23 14:46:46 +0800

    add group command auto detect

--- src/main/java/me/zhanshi123/vipsystem/manager/ConfigManager.java
+++ src/main/java/me/zhanshi123/vipsystem/manager/ConfigManager.java
@@ -8,10 +8,9 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,7 +30,7 @@ public class ConfigManager {
             Main plugin = Main.getInstance();
             f = new File(plugin.getDataFolder(), "config.yml");
             if (!f.exists()) {
-                plugin.saveResource("config.yml", false);
+                Files.copy(getConfigStream(), f.getParentFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
             }
             config.load(new BufferedReader(new InputStreamReader(new FileInputStream(f), Charsets.UTF_8)));
             dateFormat = new SimpleDateFormat(Objects.requireNonNull(config.getString("dateFormat")));
@@ -43,6 +42,15 @@ public class ConfigManager {
             e.printStackTrace();
         }
 
+    }
+
+    private InputStream getConfigStream(){
+        if(Bukkit.getPluginManager().isPluginEnabled("PermissionsEx")){
+            return Main.getInstance().getResource("config_pex.yml");
+        }else if(Bukkit.getPluginManager().isPluginEnabled("LuckPerms")){
+            return Main.getInstance().getResource("config_lp.yml");
+        }
+        return Main.getInstance().getResource("config.yml");
     }
 
     public String getLanguage() {
